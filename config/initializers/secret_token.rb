02d755152a64070e41572f3522dadcca2c9b8afc
dpantic <dpantic@gmail.com>
2012-06-19 11:42:56 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Images::Application.config.secret_token = 'd08508a94ffd5823d541b1fcc8c224ae6525d24b3d803205ef18f2cdff7153d88147b08022abd0e58cc1a6b2c37b3affcf32cced9e4cb2b486e128b8b58d8169'
